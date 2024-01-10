9a8f395a7ecb7cf0bb92e6e47e3b53a54543944d
Muhammad Saim <muhammadsaim494@gmail.com>
2024-01-10 14:48:34 +0500

    DoublyLinkedList class with constructor

---
+++ LinkedList/Doubly/DoublyLinkedList.py
@@ -0,0 +1,11 @@
+from .Node import Node
+
+'''
+A doubly linked list and its all operations
+'''
+class DoublyLinkedList:
+
+      # a class construtor
+      def __init__(self) -> None:
+            self.count = -1
+            self.head = None
