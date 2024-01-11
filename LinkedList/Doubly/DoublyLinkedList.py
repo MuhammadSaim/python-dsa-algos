16d83ffffd459366ec9bad80719a564f4b95dc54
Muhammad Saim <muhammadsaim494@gmail.com>
2024-01-11 11:51:33 +0500

    insert operation in doubly linkedlist

--- LinkedList/Doubly/DoublyLinkedList.py
+++ LinkedList/Doubly/DoublyLinkedList.py
@@ -9,3 +9,19 @@ class DoublyLinkedList:
       def __init__(self) -> None:
             self.count = -1
             self.head = None
+
+      '''
+      insert node at the end of the list
+      '''
+      def insert(self, data) -> None:
+
+            new_node = Node(data)
+
+            if self.head:
+                  current_node = self.head
+                  while(current_node):
+                        current_node = current_node.next
+                  current_node.next = new_node
+                  new_node.prev = current_node
+            else:
+                  self.head = new_node
